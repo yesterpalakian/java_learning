7af361b99b920dca9a4741ca2b13aca2f4d82c63
yesterpalakian <yesterpalakian@gmail.com>
2023-06-04 15:56:52 +0300

    Initial commit

---
+++ src/main/java/by/javarush/level01/lecture01/task01/Solution.java
@@ -0,0 +1,9 @@
+package by.javarush.level01.lecture01.task01;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String agree = "Я согласен на зарплату";
+        System.out.println(agree);
+    }
+}
